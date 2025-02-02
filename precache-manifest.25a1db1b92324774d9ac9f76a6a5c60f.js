33189ee7ef017e2be945b8aa1e8039f663405c05
christosnikolaou <xrisnik55@gmail.com>
2025-02-02 18:19:37 +0200

    Updates

---
+++ precache-manifest.25a1db1b92324774d9ac9f76a6a5c60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806e159815e63d1f3ca4af647e62f7df",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/index.html"
+  },
+  {
+    "revision": "20f410ccc555097da883",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "8cbc044ef916412a569e",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/static/css/main.873abcbd.chunk.css"
+  },
+  {
+    "revision": "20f410ccc555097da883",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/static/js/2.5d091242.chunk.js"
+  },
+  {
+    "revision": "8cbc044ef916412a569e",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/static/js/main.39de0814.chunk.js"
+  },
+  {
+    "revision": "ee8797eb700949a30fa5",
+    "url": "/ChristosNkl/crowdfyndingreactapp.git/static/js/runtime~main.83c5c7f1.js"
+  }
+]);
